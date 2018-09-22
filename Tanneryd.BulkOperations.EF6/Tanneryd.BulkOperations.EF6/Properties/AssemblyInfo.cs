9f12aecdb69b73f89b67fc5a3716a11a4919ec67
Måns Tånneryd <mans@tanneryd.se>
2018-09-22 19:58:33 +0200

    Release 1.2.1

--- Tanneryd.BulkOperations.EF6/Tanneryd.BulkOperations.EF6/Properties/AssemblyInfo.cs
+++ Tanneryd.BulkOperations.EF6/Tanneryd.BulkOperations.EF6/Properties/AssemblyInfo.cs
@@ -17,4 +17,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.2.1")]
 [assembly: AssemblyFileVersion("1.2.1")]
-[assembly: AssemblyInformationalVersion("1.2.1-rc2")]
+[assembly: AssemblyInformationalVersion("1.2.1")]
