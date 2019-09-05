a44781e5df273e637fecf88814c4169a1ec2fefb
Måns Tånneryd <mans@tanneryd.se>
2019-09-06 00:13:14 +0200

    Refactored creation of sql commands, again. Removed some obsolete code. Bumped up the version to 1.2.7, skipping 1.2.6.

--- Tanneryd.BulkOperations.EF6/Tanneryd.BulkOperations.EF6/Properties/AssemblyInfo.cs
+++ Tanneryd.BulkOperations.EF6/Tanneryd.BulkOperations.EF6/Properties/AssemblyInfo.cs
@@ -16,6 +16,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.2.6")]
-[assembly: AssemblyFileVersion("1.2.6")]
-[assembly: AssemblyInformationalVersion("1.2.6")]
+[assembly: AssemblyVersion("1.2.7")]
+[assembly: AssemblyFileVersion("1.2.7")]
+[assembly: AssemblyInformationalVersion("1.2.7")]
